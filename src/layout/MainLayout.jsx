6692265ca799b46f022994441790595df292b3a6
Banaz Software <banazsleman2004@gmail.com>
2026-03-04 15:38:31 +0300

    adding navbar

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -9,11 +9,6 @@ function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <Button className="" size="lg" variant="primary">
-        Create Account
-      </Button>
-      {/* <Input className="m-10 w-38.75 h-8" placeholder="First Name" /> */}
-
       <main className="flex-1">
         <Outlet />
       </main>
