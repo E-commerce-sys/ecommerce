ac5e921b15d4bb333c8a08c80000d7c33f883a85
Banaz Software <banazsleman2004@gmail.com>
2026-03-09 11:13:28 +0300

    added categories and subcategories integration

--- src/features/categories/categoriesAPI.js
+++ src/features/categories/categoriesAPI.js
@@ -1,7 +1,7 @@
 import axiosInstance from "../../axios/axiosInterceptor";
 
 export async function categoriesAPI() {
-  const response = await axiosInstance.get("/api/categories");
+  const response = await axiosInstance.get("/api/categories?include=parent");
 
   return response.data;
 }
