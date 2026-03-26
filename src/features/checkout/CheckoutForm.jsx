a929c596a4ac655899a8758d1af9a51316544124
Banaz Software <banazsleman2004@gmail.com>
2026-03-26 12:49:40 +0300

    checkout page added

--- src/features/checkout/CheckoutForm.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-function CheckoutForm() {
-  return <div></div>;
-}
-
-export default CheckoutForm;
--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/theme.css";
 import App from "./app/App";
+
 import { LanguageProvider } from "./context/LanguageContext.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import { CategoryProvider } from "./context/CategoryContext";
-import "./axios/axiosInterceptor.js"
+import { CartProvider } from "./context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <LanguageProvider>
         <CategoryProvider>
-          <App />
-        </CategoryProvider>{" "}
-      </LanguageProvider>{" "}
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoryProvider>
+      </LanguageProvider>
     </AuthProvider>
   </StrictMode>,
 );
