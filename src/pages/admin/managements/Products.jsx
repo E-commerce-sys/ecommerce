a36b02d7d5c61bd148d3d8986b8d3bbdd94d00ea
Aland Anees <aland.anees@gmail.com>
2026-04-14 14:01:09 +0300

    bfhsfsf

--- src/pages/admin/managements/Products.jsx
+++ src/pages/admin/managements/Products.jsx
@@ -1,9 +1,5 @@
 function Products() {
-  return <div className="min-h-screen bg-[rgb(var(--color-grey))] p-8">
-    <h1 className="text-3xl font-bold text-[rgb(var(--color-text-main))] mb-8">
-        Products
-      </h1>
-  </div>;
+  return <div>Products</div>;
 }
 
 export default Products;
