7bf41fdc8c5d12759d051ff4ad219c027fb70846
Banaz Software <banazsleman2004@gmail.com>
2026-04-15 10:18:37 +0300

    update order

--- src/features/admin/orders/Order.jsx
+++ src/features/admin/orders/Order.jsx
@@ -2,6 +2,40 @@ import Button from "@/components/Button";
 import TableDemo from "./TableDemo";
 import { useState } from "react";
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const states = [
+  {
+    id: 1,
+    name: "Pending",
+  },
+  {
+    id: 2,
+    name: "Preparing",
+  },
+  {
+    id: 3,
+    name: "Shipping",
+  },
+  {
+    id: 4,
+    name: "Delivering",
+  },
+  { id: 5, name: "Arrived" },
+  {
+    id: 6,
+    name: "Cancelled",
+  },
+];
+
 function Order() {
   const [isEditing, setIsEditing] = useState(false);
   const [selectedOrders, setSelectedOrders] = useState([]);
@@ -42,13 +76,34 @@ function Order() {
           {isEditing ? "Save" : "Edit"}
         </Button>
       </div>
-
-      <div className="w-full">
-        <TableDemo
-          isEditing={isEditing}
-          selectedOrders={selectedOrders}
-          toggleOrder={toggleOrder}
-        />{" "}
+      <div className="flex flex-col gap-4">
+        {" "}
+        <div className="flex gap-2 items-center">
+          <p>Filter by State</p>
+          <Select>
+            <SelectTrigger>
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>States</SelectLabel>
+                <SelectItem>All</SelectItem>
+                {states.map((state) => (
+                  <SelectItem key={state.id} value={String(state.name)}>
+                    {state.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="w-full">
+          <TableDemo
+            isEditing={isEditing}
+            selectedOrders={selectedOrders}
+            toggleOrder={toggleOrder}
+          />{" "}
+        </div>
       </div>
     </div>
   );
